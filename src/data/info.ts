395a0db0a07453f779ff07175188581f4f5c881d
HansonR <hanson.r@outlook.com>
2024-03-28 12:22:00 +0100

    [astro-portfolio] 1st commit

---
+++ src/data/info.ts
@@ -0,0 +1,97 @@
+export const info = {
+  baseUrl: "https://astro-portfolio-uzair.vercel.app",
+  name: "Hanson Ramkhalawon",
+  jobDescription: "Full-Stack Developer",
+  about: `I'm a software engineer based in Louvres, France.
+"I am a passionate Full Stack developer focused on creating web applications. 
+With expertise in back-end technologies such as Node.js, 
+I have a strong command of programming languages such as JavaScript and TypeScript, 
+as well as popular frameworks like Node.js, React, and Express.js. 
+I am capable of turning conceptual ideas into functional applications and I stay constantly updated with the latest technological trends. 
+I am particularly interested in the DevOps approach and the Go programming language."`,
+
+  experience: [
+    {
+      name: "Atos SE",
+      location: "Ingénieur FullStack | Bezons 95870",
+      startDate: "february 2023",
+      endDate: "Present",
+      description: [
+        "✓ Bug resolution in Java and Go on multiple Jira tickets",
+        "✓ Creating an Nginx configuration script for amazon linux v2 and v3 machines",
+      ],
+    },
+
+    {
+      name: "Serimax Holding",
+      location: "Architect Cloud | Roissy 95700",
+      startDate: "April 2020",
+      endDate: "October 2022",
+      description: [
+        "✓ In the digitization division, I was in charge of maintaining the data manager application in electron.js",
+        "✓ back-end maintenance and creation with node.js",
+        "✓ pipeline creation with gitlab",
+        "✓ AWS resource creation via terraform",
+      ],
+    },
+
+    {
+      name: "Alten : Renault & Orange",
+      location: "Ingénieur DevOps | Ile de France",
+      startDate: "June 2021",
+      endDate: "March 2022",
+      description: [
+        "✓ Creation of a load balancer with python for the 3visu application",
+        "✓ application maintenance with PHP and JavaScript",
+      ],
+    },
+
+    {
+      name: "GFI informatique : Thales",
+      location: "Consultant JAVA/PLM | Meudon 92190",
+      startDate: "January 2018",
+      endDate: "November 2020",
+      description: [
+        "✓ Developing code in accordance with appropriate design and architectural rules : JAVA WindChill/PLM",
+        "✓ JavaScript and PHP developement and documentation",
+        "✓ Management of linux virtual machines, snapshot creation for Thales team",
+        "✓ JIRA ticketing, Backlog creation for the Indian team",
+        "✓ Code Improvements and Refactoring ",
+        "✓ Best Practices and Rules Maintainer",
+      ],
+    },
+  ],
+
+  education: [
+    {
+      name: "CNAM - master II",
+      location: "Paris, France",
+      startDate: "2018",
+      endDate: "2020",
+      description: ["software architecture designer.",],
+    },
+    {
+      name: "Doranco - BTS SIO",
+      location: "Paris, France",
+      startDate: "2017",
+      endDate: "2018",
+      description: ["multimedia application developer"],
+    },
+  ],
+
+  socialMedia: {
+    github: "https://github.com/uzzii-21",
+    email: "mailto:uzairahmed@74372gmail.com",
+    linkedin: "https://www.linkedin.com/in/uzzii21/",
+  },
+
+  projects: [
+    {
+      title: "Astro Portfolio",
+      isFeatured: true,
+      thumbnail: "/assets/images/astro-portfolio.png",
+      githubUrl: "https://github.com/uzzii-21/astro-portfolio",
+      liveUrl: "https://astro-portfolio-uzair.vercel.app/",
+    },
+  ],
+};
